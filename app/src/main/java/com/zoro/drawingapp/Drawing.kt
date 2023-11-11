0a6cbeb5f82fef472e5f8c60e0994716c04a29e1
Manishk031 <khandelwalmanish582@gmail.com>
2023-11-11 07:36:32 +0530

    The line persist on the view

--- app/src/main/java/com/zoro/drawingapp/Drawing.kt
+++ app/src/main/java/com/zoro/drawingapp/Drawing.kt
@@ -19,6 +19,7 @@ class Drawing (context: Context, attrs:AttributeSet): View(context,attrs){
     private var mBrushSize:Float = 0.toFloat()
     private var color =Color.BLACK
     private var canvas:Canvas? = null
+    private val mPath =ArrayList<CustomPath>()
 
 
 
@@ -50,11 +51,20 @@ class Drawing (context: Context, attrs:AttributeSet): View(context,attrs){
         super.onDraw(canvas)
          canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
 
+                  for(path in mPath)
+                  {
+                      mDrawPaint!!.strokeWidth = path.brushThickness
+                      mDrawPaint!!.color = path.color
+                      canvas.drawPath(path,mDrawPaint!!)
+                  }
+
                   if(!mDrawPath!!.isEmpty)
                   {
                       mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
                       mDrawPaint!!.color = mDrawPath!!.color
                       canvas.drawPath(mDrawPath!!,mDrawPaint!!)
+
+
                   }
 
                   }
@@ -85,6 +95,7 @@ class Drawing (context: Context, attrs:AttributeSet): View(context,attrs){
             }
 
             MotionEvent.ACTION_UP ->{
+                mPath.add(mDrawPath!!)
                 mDrawPath = CustomPath(color,mBrushSize)
             }
             else -> return false
