479520754f3f483baa2790de947f5c447da09e44
Manishk031 <khandelwalmanish582@gmail.com>
2023-11-11 15:18:40 +0530

    select the brush size

--- app/src/main/java/com/zoro/drawingapp/Drawing.kt
+++ app/src/main/java/com/zoro/drawingapp/Drawing.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -38,7 +39,7 @@ class Drawing (context: Context, attrs:AttributeSet): View(context,attrs){
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize =20.toFloat()
+       // mBrushSize =20.toFloat() // Here the default the initial
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -104,6 +105,12 @@ class Drawing (context: Context, attrs:AttributeSet): View(context,attrs){
 
 
         return true
+    }
+
+    fun setSizeForBrush(newSize:Float){
+          mBrushSize  = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+              newSize,resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
             internal inner class CustomPath(var color: Int, var brushThickness:Float): Path() {
 
--- app/src/main/java/com/zoro/drawingapp/MainActivity.kt
+++ app/src/main/java/com/zoro/drawingapp/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.zoro.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private  var drawingView : Drawing? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        drawingView = findViewById(R.id.Drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        val ib_brush: ImageButton= findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+
+            }
+       private   fun showBrushSizeChooserDialog()
+       {
+           var brushDialog = Dialog(this)
+           brushDialog.setContentView(R.layout.dialog_brush_size)
+           brushDialog.setTitle("Brush Size: ")
+
+               // small btn
+               val smallBtn :ImageButton = brushDialog.findViewById(R.id.small_brush)
+               smallBtn.setOnClickListener{
+               drawingView?.setSizeForBrush(10.toFloat())
+               brushDialog.dismiss()
+           }
+           val mediumBtn :ImageButton = brushDialog.findViewById(R.id.medium_brush)
+           mediumBtn.setOnClickListener{
+               drawingView?.setSizeForBrush(20.toFloat())
+               brushDialog.dismiss()
+           }
+           val largeBtn :ImageButton = brushDialog.findViewById(R.id.large_brush)
+           largeBtn.setOnClickListener{
+               drawingView?.setSizeForBrush(30.toFloat())
+               brushDialog.dismiss()
+           }
+           brushDialog.show()
+       }
 }
